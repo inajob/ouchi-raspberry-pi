e995d23a09ae1cca97b1009075f105be5f7c0751
inajob <ina_job@hotmail.com>
2021-12-05 21:43:29 +0900

    add temperature data in schedule

--- web-signage/daily.sh
+++ web-signage/daily.sh
@@ -4,3 +4,6 @@ cd `dirname $0`
 mkdir out
 sh makePhoto.sh > out/photo.js
 python3 get.py  > out/schedule.js
+curl https://www.jma.go.jp/bosai/forecast/data/forecast/010000.json > out/weather.json
+echo "var weather = " > out/weather.js
+cat out/weather.json >> out/weather.js
