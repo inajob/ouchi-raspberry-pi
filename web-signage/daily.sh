5ca64aea7a1ad471dac3e6493f28c726ffb758fe
inajob <ina_job@hotmail.com>
2021-12-06 20:10:50 +0900

    show disk info

--- web-signage/daily.sh
+++ web-signage/daily.sh
@@ -7,3 +7,6 @@ python3 get.py  > out/schedule.js
 curl https://www.jma.go.jp/bosai/forecast/data/forecast/010000.json > out/weather.json
 echo "var weather = " > out/weather.js
 cat out/weather.json >> out/weather.js
+
+echo "var disk = " > out/disk.js
+jq -n --arg f "`df -h /media/mystorage|tail -1|awk '{print $3 "/" $2 " " $5}'`" '.file = $f' >> out/disk.js
