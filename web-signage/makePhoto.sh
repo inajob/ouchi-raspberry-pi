b555ea50f3c08827546d2cfcea3f814226c37fa5
inajob <ina_job@hotmail.com>
2023-03-05 19:51:28 +0900

    support filepath include space

--- web-signage/makePhoto.sh
+++ web-signage/makePhoto.sh
@@ -1,7 +1,9 @@
 PHOTO_PATH=$(cat ../secrets/photo-path)
 echo "var photoList = ["
+IFS='
+'
 for line in `find /media/mystorage/${PHOTO_PATH} -type f|grep -e '.*\.\(HEIC\|jpg\|JPG\)'`; do
-  d=`ls --full-time $line | awk '{print $6 " " $7}'`
+  d=`ls --full-time "$line" | awk '{print $6 " " $7}'`
 
   # d=`exiftool $line|grep "Create Date"|head -1|awk '{gsub(":","/",$4); print $4 " " $5}'`
   echo "{'name':'$line', 'date':'$d'},";
