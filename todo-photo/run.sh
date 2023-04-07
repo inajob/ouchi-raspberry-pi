b525483eecae46e15ee33180c28431ddd893e50c
inajob <ina_job@hotmail.com>
2023-04-07 09:42:32 +0900

    set resolution and capture 3 times

--- todo-photo/run.sh
+++ todo-photo/run.sh
@@ -2,7 +2,15 @@
 
 cd `dirname $0`
 
+# set UXGA
+curl $(cat ../secrets/todo-camera-address)/control?var=framesize&val=10
+sleep 1
 curl $(cat ../secrets/todo-camera-address)/capture -o todo.jpg
+sleep 1
+curl $(cat ../secrets/todo-camera-address)/capture -o todo.jpg
+sleep 1
+curl $(cat ../secrets/todo-camera-address)/capture -o todo.jpg
+
 convert todo.jpg -rotate 180 -fill white -pointsize 25 -font Helvetica -draw "text 20,20 '`date`'" -background white todo.jpg
 
 for f in $(rclone --config=../secrets/rclone.conf lsf "google drive:/ouchi-raspberry-pi/"); do
